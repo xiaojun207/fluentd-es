be45f4c1472974d6cf7979768bf2e8cdf8d97220
name <email>
2020-07-16 22:13:33 +0800

    内置配置文件fluent.conf（包括推向elasticsearch, java log4j日志多行文件的支持等）

--- add-test-log-2.sh
+++ add-test-log-2.sh
@@ -1,4 +1,6 @@
 for i in $(seq 1 9);do
-  echo "2020-07-16 21:0${i}:15,076 ERROR com.g.job.TestJob 29 error.trace" >> logs/common-info.log;
+  echo "2020-07-16 21:0${i}:15,076 INFO com.g.job.TestJob 23 INFO.trace" >> logs/common-info.log;
+  echo "2020-07-16 21:0${i}:25,076 ERROR com.g.job.TestJob 29 error.trace" >> logs/common-error.log;
   sleep 1;
 done;
+
