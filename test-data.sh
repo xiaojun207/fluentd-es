c3eaacf036b3e0ae7f3c53bc68d63e1f105d7702
name <email@example.com>
2022-06-07 10:00:48 +0800

    add fluentd to zinc conf

---
+++ test-data.sh
@@ -0,0 +1,3 @@
+curl -i -X POST -d 'json={"log":"\u001b[2m2021-06-16 01:55:58.624\u001b[0;39m \u001b[32m INFO\u001b[0;39m \u001b[35m8\u001b[0;39m \u001b[2m---\u001b[0;39m \u001b[2m[           main]\u001b[0;39m \u001b[36mo.s.b.w.embedded.tomcat.TomcatWebServer \u001b[0;39m \u001b[2m:\u001b[0;39m Tomcat started on port(s): 8021 (http) with context path ''\r\n","stream":"stdout","time":"2021-06-16T01:55:58.627001397Z"}' http://localhost:24224/test.data
+
+
---
+++ test.sh
@@ -0,0 +1,1 @@
+docker run -d --name test2 busybox /bin/sh -c "while true; do sleep 2; echo '2021-06-12 16:26:52,398 ERROR o.a.c.ConnectionState'; done"
